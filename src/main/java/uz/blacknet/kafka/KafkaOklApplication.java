7712a5a3906d7c83825eedcff438e0c5f948b3c2
root <theferganes@gmail.com>
2025-01-01 20:25:38 +0500

    1st

---
+++ src/main/java/uz/blacknet/kafka/KafkaOklApplication.java
@@ -0,0 +1,13 @@
+package uz.blacknet.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOklApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOklApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/blacknet/kafka/KafkaOklApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.blacknet.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOklApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
