3ca5c12903a1c97bf418d5b1463fd75039f10334
root <theferganes@gmail.com>
2025-01-05 18:33:06 +0500

    test success

--- src/main/java/uz/blacknet/kafka/KafkaOklApplication.java
+++ src/main/java/uz/blacknet/kafka/KafkaOklApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaOklApplication {
+public class KafkaOklApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaOklApplication.class, args);
-	}
+    public static void main(String[] args)
+    {
+        SpringApplication.run(KafkaOklApplication.class, args);
+    }
 
 }
---
+++ src/main/java/uz/blacknet/kafka/config/Runner.java
@@ -0,0 +1,23 @@
+package uz.blacknet.kafka.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+
+@Component
+public class Runner implements CommandLineRunner
+{
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        for (int i = 0; i < 10_000_000; i++)
+        {
+            kafkaTemplate.send("ok", "I love you Zakkunai " + i);
+        }
+    }
+}
